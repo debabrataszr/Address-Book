37ecd4a03c518d17cd88e3f2bf6ff3b683ae6095
debabrata-97 <debabrataszr@gmail.com>
2023-03-04 20:06:13 +0530

    Create a Contact in Address Book

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,5 +1,25 @@
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        Contact c = new Contact();
+
+        c.firstname = "Debabrata";
+        c.lastname = "Sahoo";
+        c.address = "Koel Nagar";
+        c.city = "Rourkela";
+        c.state = "Odisha";
+        c.email = "debabratas@xyz.com";
+        c.zip = 751025;
+        c.phoneno = 797805896;
+
+        System.out.println("First name:"+c.firstname);
+        System.out.println("Last name:"+c.lastname);
+        System.out.println("Addres:"+c.address);
+        System.out.println("City:"+c.city);
+        System.out.println("State:"+c.state);
+        System.out.println("E-mail:"+c.email);
+        System.out.println("Zip code:"+c.zip);
+        System.out.println("Phone no.:"+c.phoneno);
     }
 }
